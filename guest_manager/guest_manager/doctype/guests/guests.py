f907a47b6a63e761d0e4c3d2186793d8347277a4
Victor Maduforo <victor.maduforo@manqala.com>
2021-02-18 14:03:37 +0100

    drafting reports

--- guest_manager/guest_manager/doctype/guests/guests.py
+++ guest_manager/guest_manager/doctype/guests/guests.py
@@ -10,7 +10,7 @@ from frappe.model.document import Document
 class Guests(Document):
 	def before_save(self):
 		self.full_name = f'{self.guest_first_name} {self.guest_last_name or ""}'
-		self.address = frappe.db.get_single_value('Resident','address')
+		# self.address = frappe.db.get_single_value('Resident','address')
 		if self.expected_arrival_time < frappe.utils.nowdate():
 			frappe.throw('Arrival time should not be later than current time')
 		if self.expected_departure_time:
@@ -20,8 +20,10 @@ class Guests(Document):
 				frappe.throw('Arrival time should not be same as departure time')
 	def on_submit(self):
 		visit = frappe.new_doc('Visits')
-		visit.resident = frappe.db.get_single_value('Resident','full_name')
-		visit.address = frappe.db.get_single_value('Resident','address')
+		# visit.resident = frappe.db.get_single_value('Resident','full_name')
+		visit.resident = self.resident
+		# visit.address = frappe.db.get_single_value('Resident','address')
+		visit.address = self.address
 		visit.guest = self.full_name
 		visit.expected_arrival = self.expected_arrival_time
 		visit.expected_departure = self.expected_departure_time
--- guest_manager/guest_manager/doctype/resident/resident.py
+++ guest_manager/guest_manager/doctype/resident/resident.py
@@ -3,10 +3,13 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class Resident(Document):
 	def before_save(self):
 		self.full_name = f'{self.first_name} {self.last_name or ""}'
+		self.email = frappe.session.user
+	def on_submit(self):
+		pass
 	# pass
